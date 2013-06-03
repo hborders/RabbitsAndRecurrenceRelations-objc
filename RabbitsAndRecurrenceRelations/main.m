57b9b3dfddfd4992b12956420d4487bc0a0b2241
Heath Borders <heath.borders@jivesoftware.com>
2013-06-02 22:29:50 -0500

    Default Xcode Foundation Project without man page.

---
+++ RabbitsAndRecurrenceRelations/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RabbitsAndRecurrenceRelations
+//
+//  Created by Heath Borders on 6/2/13.
+//  Copyright (c) 2013 Heath Borders. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
